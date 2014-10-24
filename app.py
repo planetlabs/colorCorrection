26f3ff9412be4e22747019b56bf0e9cee929c089
matt0 <matt@planet.com>
2014-10-24 12:15:30 -0700

    added ol.js + ol.css, have basic cat image embedded

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return flask.render_template("index.html")
+    return flask.render_template(
+    	"index.html",
+    	image_name="cat.tif")
 
 if __name__ == '__main__':
     app.run()
--- static/js/colorify.js
+++ static/js/colorify.js
@@ -1,4 +1,30 @@
 
-function say_hi() {
-	console.log("hi");
+function makeImage(url) {
+	var extent = [0, 0, 3072, 2600];
+    var pixelProjection = new ol.proj.Projection({
+        code: 'image',
+        units: 'pixels',
+        extent: extent
+    });
+
+	var imageLayer = new ol.layer.Image({
+	  	source: new ol.source.ImageStatic({
+		    url: url,
+		    projection: pixelProjection,
+		    imageExtent: extent
+	  	})
+	});
+
+	imageMap = new ol.Map({
+	    target:'olImage',
+	    view: new ol.View({
+		    projection: pixelProjection,
+		    center:ol.extent.getCenter(extent),
+		    zoom: 1.5
+	    })
+	});
+	imageMap.addLayer(imageLayer);
+
 }
+
+
