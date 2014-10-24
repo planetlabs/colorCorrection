a9edf4cd78f5cba8d4b74b171314f4f91f8b2f2f
matt0 <matt@planet.com>
2014-10-24 14:43:31 -0700

    introduced level curve functionality

--- static/js/colorify.js
+++ static/js/colorify.js
@@ -26,43 +26,75 @@ function makeImage(url) {
 	});
 	imageMap.addLayer(imageLayer);
 
-	imageLayer.on("postcompose", function(event) {
-		console.log("POST", event);
-
-		var resolution = event.frameState.viewState.resolution;
-		var context = event.context;
-		var canvas = context.canvas;
-		var width = canvas.width;
-		var height = canvas.height;
-
-		var imageData = context.getImageData(0, 0, width, height);
-  		var data = imageData.data;
-
-  		var threshold = 157;
-  		var offset, r, g, b, a, m;
-  		for (var i = 0; i < width; ++i) {
-		    for (var j = 0; j < height; ++j) {
-			    offset = 4 * (j * width + i);
-			    a = data[offset + 3];
-			    if (a > 0) {
-			        r = data[offset] / 255;
-			        g = data[offset + 1] / 255;
-			        b = data[offset + 2] / 255;
-			        m = (r + g + b) / 3;
-			        if (m < .5) {
-			            data[offset] = 0;
-			            data[offset + 1] = 155;
-			            data[offset + 2] = 0;
-			            data[offset + 3] = 255;
-			        }
-			    }
-		    }
-    	}
-
-  		context.putImageData(imageData, 0, 0);
-
-	})
-
+	imageLayer.on("postcompose", postcompose);
 }
 
+
+function identityCurve(originalValue) {
+	return originalValue;
+}
+
+function levelCurve(min, max) {
+	// Returns a function that takes in and returns 1 float, that maps
+	// an intensity value from the space [0-255] back into the space [0-255]
+	// but thresholding below the min and above the max, with linear
+	// interpolation between. This is known as color level adjustment
+
+	// This is the gain/offset that describes our linear interpolation
+	var m = 255.0 / (max - min);
+	var b = -m * min;
+
+	function newCurve(originalValue) {
+		if (originalValue > max) {
+			return 255;
+		}
+		else if (originalValue < min) {
+			return 0;
+		}
+		else {
+			return originalValue * m + b;
+		}
+	}
+
+	return newCurve
+}
+
+
+redCurve = levelCurve(50, 225);
+greenCurve = levelCurve(50, 225);
+blueCurve = levelCurve(50, 225);
+
+
+function postcompose(event) {
+	var resolution = event.frameState.viewState.resolution;
+	var context = event.context;
+	var canvas = context.canvas;
+	var width = canvas.width;
+	var height = canvas.height;
+
+	var imageData = context.getImageData(0, 0, width, height);
+	var data = imageData.data;
+
+	var offset, a;
+	for (var i = 0; i < width; ++i) {
+	    for (var j = 0; j < height; ++j) {
+		    offset = 4 * (j * width + i);
+		    a = data[offset + 3];
+		    if (a > 0) {
+		    	// r
+		        data[offset] = redCurve(data[offset]);
+
+		        // g
+		        data[offset + 1] = greenCurve(data[offset + 1]);
+
+		        // b
+		        data[offset + 2] = blueCurve(data[offset + 2]);
+		    }
+
+
+	    }
+	}
+
+	context.putImageData(imageData, 0, 0);
+}
 
