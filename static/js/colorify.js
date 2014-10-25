821975eb01369dea8ef18c82d566de6bd02a7321
matt0 <matt@planet.com>
2014-10-24 17:09:14 -0700

    histograms displaying!

--- static/js/colorify.js
+++ static/js/colorify.js
@@ -31,12 +31,13 @@ function makeImage(url) {
 }
 
 function levelCurve(min, max) {
-	// Returns a function that takes in and returns 1 float, that maps
+	// Returns a function that takes in and returns 1 float. Function maps
 	// an intensity value from the space [0-255] back into the space [0-255]
 	// but thresholding below the min and above the max, with linear
 	// interpolation between. This is known as color level adjustment
 
-	// This is the gain/offset that describes our linear interpolation
+	// This is the gain/offset that describes our linear interpolation.
+	// Good ol' y=mx+b
 	var m = 255.0 / (max - min);
 	var b = -m * min;
 
@@ -60,7 +61,6 @@ redCurve = levelCurve(0, 225);
 greenCurve = levelCurve(0, 225);
 blueCurve = levelCurve(0, 225);
 
-
 function postcompose(event) {
 	var resolution = event.frameState.viewState.resolution;
 	var context = event.context;
@@ -77,20 +77,42 @@ function postcompose(event) {
 		    offset = 4 * (j * width + i);
 		    a = data[offset + 3];
 		    if (a > 0) {
-		    	// r
-		        data[offset] = redCurve(data[offset]);
-
-		        // g
-		        data[offset + 1] = greenCurve(data[offset + 1]);
-
-		        // b
-		        data[offset + 2] = blueCurve(data[offset + 2]);
+		        data[offset] = redCurve(data[offset]); // r
+		        data[offset + 1] = greenCurve(data[offset + 1]); //g
+		        data[offset + 2] = blueCurve(data[offset + 2]); //b
 		    }
-
-
 	    }
 	}
 
 	context.putImageData(imageData, 0, 0);
 }
 
+
+function newFilledArray(length, val) {
+	// as copied from http://stackoverflow.com/a/1296684/672601
+    var array = [];
+    for (var i = 0; i < length; i++) {
+        array[i] = val;
+    }
+    return array;
+}
+
+function makeHistogram(data, width, height) {
+	var red = newFilledArray(255, 0);
+	var green = newFilledArray(255, 0);
+	var blue = newFilledArray(255, 0);
+
+	var offset, a;
+	for (var i = 0; i < width; ++i) {
+	    for (var j = 0; j < height; ++j) {
+		    offset = 4 * (j * width + i);
+		    a = data[offset + 3];
+		    if (a > 0) {
+		        red[Math.floor(data[offset])] += 1;
+		        green[Math.floor(data[offset + 1])] += 1;
+		        blue[Math.floor(data[offset + 2])] += 1;
+		    }
+	    }
+	}
+	return [red, green, blue];
+}
